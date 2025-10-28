bf6de44fea4406f02167aee2fb918b8c08ffe607
logidhasan <logidhasan25@gmail.com>
2025-10-28 11:06:06 +0530

    test file update with data extraction and transmission

--- test/python/test_db_connection.py
+++ test/python/test_db_connection.py
@@ -1,74 +1,47 @@
-import sys
 import os
-import pandas as pd
+import sys
 from pathlib import Path
 from sqlalchemy import text
+
 sys.path.append(os.path.abspath("paper_curator/"))
 
 from src.db.interfaces.postgresql import PostgreSQLDatabase
 from src.schemas.database.config import PostgreSQLSettings
 from src.extractor.dbextractor import DBExtractor
 from src.transmission import DataTransmitter
-from src.storage import ESConnector
+from src.elasticsearch.connection import ElasticsearchConfig, ElasticsearchClientFactory
 
-config = PostgreSQLSettings(
-    database_url="postgresql://rag_user:rag_password@localhost:5432/paper_curator",
-    echo_sql=True,
-)
-
-db = PostgreSQLDatabase(config)
-
-db.startup()
-print("database connection successful")
-
-with db.get_session() as session:
-    session.execute(
-        text("""
-        CREATE TABLE IF NOT EXISTS students (
-             id SERIAL PRIMARY KEY,
-             name VARCHAR(50),
-             department VARCHAR(50),
-             marks INTEGER
-        )
-        """)
+def setup_database():
+    """ 
+     Create table and insert sample data if missing.
+    """
+    config = PostgreSQLSettings(
+        database_url="postgresql://rag_user:rag_password@localhost:5432/paper_curator",
+        echo_sql=True,
     )
-    session.commit()
 
-    result = session.execute(text("SELECT COUNT(*) FROM students"))
-    count = result.scalar()
+    db = PostgreSQLDatabase(config)
+    db.startup()
+    print("Database connection successful")
+    return db
 
-    if count == 0:
-        print("Inserting sample records...")
-        session.execute(
-            text("""
-            INSERT INTO students (name, department, marks)
-                 VALUES
-                 ('Logidhasan', 'AI', 95)
-                 """)
-        )
-        session.commit()
-        print("sample records inserted.")
-    else:
-        print("sample data already exists.")
+def extract_data(db):
+    extractor = DBExtractor(db)
+    df = extractor.extract_table("students", to_csv=True)
+    print("\nExtracted DataFrame: ")
+    print(df)
+    print("\nData successfully extracted and exported to 'students_export.csv'")
+    return df
 
-extractor = DBExtractor(db)()
-df = extractor.extract_table("students", to_csv=True)
+def transmission(df):
+    json_output_path = Path("students_export.json")
+    transmitter = DataTransmitter(df, save_to_file=True, output_path=json_output_path)
+    json_str = transmitter()
+    print("\nTransmitter JSON string: ")
+    print(json_str)
 
-print("\nExtracted dataframe:")
-print(df)
 
-print("\nData successfully extracted and exported to 'students_export.csv'")
-
-json_output_path = Path("students_export.json")
-transmitter = DataTransmitter(df, save_to_file=True, output_path=json_output_path)
-json_str = transmitter()
-print("\nTransmitter JSON string:")
-print(json_str)
-
-es_storage = ESStorage(host="localhost", port=9200)
-es_storage.startup()
-
-es_storage.insert_json(index_name="students", json_str=json_str)
-
-db.teardown()
-print("Database connection closed.")
+if __name__ == "__main__":
+    db = setup_database()
+    df = extract_data(db)
+    transmission(df)
